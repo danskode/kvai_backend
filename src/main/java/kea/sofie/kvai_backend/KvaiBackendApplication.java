5fe5c9d2c3abe53c8ba78e99528b20049a825a2a
kvetny <sokv0001@stud.kea.dk>
2025-04-02 16:12:31 +0200

    Initial commit

---
+++ src/main/java/kea/sofie/kvai_backend/KvaiBackendApplication.java
@@ -0,0 +1,13 @@
+package kea.sofie.kvai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KvaiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KvaiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kea/sofie/kvai_backend/KvaiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.sofie.kvai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KvaiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
