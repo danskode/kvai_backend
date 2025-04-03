d86c6fc6032503e812c6721fed304c538848763a
kvetny <sokv0001@stud.kea.dk>
2025-04-03 12:33:31 +0200

    commit

--- src/main/java/kea/sofie/kvai_backend/controller/ChatController.java
+++ src/main/java/kea/sofie/kvai_backend/controller/ChatController.java
@@ -1,11 +1,16 @@
 package kea.sofie.kvai_backend.controller;
 
 
+import kea.sofie.kvai_backend.model.ChatResponse;
+import kea.sofie.kvai_backend.model.MessageRequest;
+import kea.sofie.kvai_backend.model.Politician;
 import kea.sofie.kvai_backend.service.ChatService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@CrossOrigin("*")
 @RestController
 public class ChatController {
 
@@ -15,8 +20,43 @@ public class ChatController {
         this.chatService = chatService;
     }
 
-    @GetMapping("/chat")
-    public String chatWithAPolitician(@RequestParam String message) {
-        return chatService.getAIResponse(message);
+    @PostMapping("/chat")
+    public ResponseEntity<ChatResponse> chatWithAPolitician(@RequestBody MessageRequest messageRequest) {
+        String politician = messageRequest.getPolitician();
+
+        if (politician != null && !politician.isEmpty()) {
+            chatService.setPolitician(politician);
+        }
+        String aiResponse = chatService.getAIResponse(messageRequest.getMessage());
+        return ResponseEntity.ok(new ChatResponse(aiResponse));
     }
+
+//    @PostMapping("/chat") //ændret fra GET til POST
+//    public String chatWithAPolitician(@RequestParam String message) {
+//        return chatService.getAIResponse(message);
+//    }
+
+//    @GetMapping("/chat")
+//    public String chatWithAPolitician(@RequestParam String message) {
+//        return chatService.getAIResponse(message);
+//    }
+
+
+    // Hardkodede et par kandidater med billeder som en test
+//    @GetMapping("/politikerne")
+//    public List<Politician> getPoliticians() {
+//        return List.of(
+//                new Politician("Mette Frederiksen", "/images/mettefrederiksen.jpeg"),
+//                new Politician("Pernille Rosenkrantz-Theil", "/images/pernillerosenkrantz-theil.jpeg"),
+//                new Politician("Jakob Næsager", "/images/jakobnæsager.jpeg"),
+//                new Politician("Mia Nyegaard", "/images/mianyegaard.jpeg")
+//        );
+//    }
+
+    @PostMapping("/vælg-politiker")
+    public String selectPolitician(@RequestParam String name) {
+        chatService.setPolitician(name);
+        return "Du chatter nu med " + name;
+    }
+
 }
---
+++ src/main/java/kea/sofie/kvai_backend/model/Politician.java
@@ -0,0 +1,23 @@
+package kea.sofie.kvai_backend.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Politician {
+
+    // Et ID ifht. db?
+
+
+    private String name;
+    private String party;
+    private String description; // evt noget kort info om politikeren?
+    private String imageUrl;
+
+}
